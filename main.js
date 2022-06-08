b6def0696fd1b87d5da3884882ddfd8272386eb7
Jacobbshapiro <jacobbshapiro7@gmail.com>
2022-06-08 13:41:49 -0700

    deploy

--- main.js
+++ main.js
@@ -4,10 +4,10 @@
 
 // Variables
 
-const arrCards = ['cA','c02','c03','c04','c05','c06','c07','c08','c09','c10','cJ','cQ','cK','dA','d02','d03','d04','d05','d06','d07','d08','d09','d10','dJ','dQ','dK','hA','h02','h03','h04','h05','h06','h07','h08','h09','h10','hJ','hQ','hK','sA','s02','s03','s04','s05','s06','s07','s08','s09','s10','sJ','sQ','sK'];
+const arrCards = ['cA', 11,'c02', 2,'c03', 3,'c04', 4,'c05', 5,'c06', 6,'c07', 7,'c08', 8,'c09', 9,'c10', 10, 'cJ', 10,'cQ', 10,'cK', 10,'dA', 11,'d02', 2,'d03', 3,'d04', 4,'d05', 5,'d06', 6,'d07', 7,'d08', 8,'d09', 9,'d10', 10,'dJ', 10,'dQ', 10,'dK', 10,'hA', 11,'h02', 2,'h03', 3,'h04', 4,'h05', 5,'h06', 6,'h07', 7,'h08', 8,'h09', 9,'h10', 10,'hJ', 10,'hQ', 10,'hK', 10,'sA', 11,'s02', 2,'s03', 3,'s04', 4,'s05', 5,'s06', 6,'s07', 7,'s08', 8,'s09', 9,'s10', 10,'sJ', 10,'sQ', 10,'sK', 10];
 
 let idxDeck = 0; 
-
+let score = 0;
 
 
 // Cached element referances
@@ -32,13 +32,21 @@ function shuffleArray (arrCards) {
 function displayCard() {
     const button = document.getElementById('deal').disabled = true
     const shuffle = document.getElementById('shuffle').disabled = false
+    const hit = document.getElementById('hit').disabled = false
 
     let card1 = document.getElementById('card0').classList;
     card1.add(arrCards[idxDeck]);
     idxDeck++;
+    
+    score=score+arrCards[idxDeck];
+    idxDeck++
+    
     let card2 = document.getElementById('card1').classList;
     card2.add(arrCards[idxDeck]);
     idxDeck++;
+    
+    score=score+arrCards[idxDeck];
+    idxDeck++
 }
 
 function reset() {
@@ -54,4 +62,7 @@ function hit() {
     let newCard = document.getElementById('card' + idxDeck).classList;
     newCard.add(arrCards[idxDeck]);
     idxDeck++;
+    
+    score=score+arrCards[idxDeck];
+    idxDeck++
 }
