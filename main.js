c00d4db97559dc2c5006d4c05bafa1aa232506d0
Jacobbshapiro <jacobbshapiro7@gmail.com>
2022-06-08 12:41:09 -0700

    hit button adds a card

--- main.js
+++ main.js
@@ -5,8 +5,8 @@
 // Variables
 
 const arrCards = ['cA','c02','c03','c04','c05','c06','c07','c08','c09','c10','cJ','cQ','cK','dA','d02','d03','d04','d05','d06','d07','d08','d09','d10','dJ','dQ','dK','hA','h02','h03','h04','h05','h06','h07','h08','h09','h10','hJ','hQ','hK','sA','s02','s03','s04','s05','s06','s07','s08','s09','s10','sJ','sQ','sK'];
-let idxDeck = 0; 
 
+let idxDeck = 0; 
 
 
 
@@ -33,10 +33,10 @@ function displayCard() {
     const button = document.getElementById('deal').disabled = true
     const shuffle = document.getElementById('shuffle').disabled = false
 
-    let card1 = document.getElementById('card1').classList;
+    let card1 = document.getElementById('card0').classList;
     card1.add(arrCards[idxDeck]);
     idxDeck++;
-    let card2 = document.getElementById('card2').classList;
+    let card2 = document.getElementById('card1').classList;
     card2.add(arrCards[idxDeck]);
     idxDeck++;
 }
@@ -46,5 +46,12 @@ function reset() {
 }
 
 function hit() {
-    document.createElement('div')
+    const div = document.createElement('div')
+    div.setAttribute('id','card' + idxDeck);
+    div.classList.add('card');
+    const hitMe = document.getElementById('your-cards')
+    hitMe.appendChild(div);
+    let newCard = document.getElementById('card' + idxDeck).classList;
+    newCard.add(arrCards[idxDeck]);
+    idxDeck++;
 }
